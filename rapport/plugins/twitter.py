5a28be473a6df922fa42ab2d60f834e98fca16f5
Sascha Peilicke <saschpe@suse.de>
2013-04-09 15:17:54 +0200

    Implemented twitter plugin

--- rapport/plugins/twitter.py
+++ rapport/plugins/twitter.py
@@ -18,15 +18,37 @@
 Twitter plugin.
 """
 
+import tweepy
+
 import rapport.plugin
 
 
 class TwitterPlugin(rapport.plugin.Plugin):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret, *args, **kwargs):
         super(TwitterPlugin, self).__init__(*args, **kwargs)
 
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+
+        self.api = tweepy.API(auth)
+
+    def _get(self, timeline, timeframe):
+        #TODO: Implement proper pagination instead of fixed count:
+        count = 50
+        statuses = []
+        for status in timeline(count=count):
+            if timeframe.contains(status.created_at):
+                statuses.append(status)
+        return statuses
+
     def collect(self, timeframe):
-        return self._results()
+        mentions = self._get(self.api.mentions_timeline, timeframe)
+        tweets = self._get(self.api.user_timeline, timeframe)
+        retweets = self._get(self.api.retweets_of_me, timeframe)
+
+        return self._results({"mentions": mentions,
+                              "tweets": tweets,
+                              "retweets": retweets})
 
 
 rapport.plugin.register("twitter", TwitterPlugin)
