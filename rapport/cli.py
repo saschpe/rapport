b1ca91a319b92be90f4449565470e95b55b90c61
Sascha Peilicke <saschpe@suse.de>
2013-04-09 12:19:58 +0200

    Make use of recent config/plugin/timeframe improvements

--- rapport/cli.py
+++ rapport/cli.py
@@ -30,78 +30,65 @@ if __name__ == "__main__":
 import rapport
 import rapport.config
 import rapport.plugin
+import rapport.timeframe
 
 
-class RapportCLI(object):
+rapport.config.load()
+rapport.plugin.discover()
+
+
+class CLI(object):
     def __init__(self):
-        rapport.plugin.discover()
-        rapport.config.configure()
+        self.plugins = rapport.plugin.init_from_config()
+        self.timeframe = rapport.timeframe.init_from_config()
 
-        self.plugins = []
-        for plugin in rapport.config.plugins():
-            self.plugins.append(rapport.plugin.init(**plugin))
+    def collect(self):
+        for plugin in self.plugins:
+            result_json = plugin.collect(self.timeframe)
+            print "Result for {0}: {1}".format(plugin.alias, result_json)
 
-    def _collect(self):
-
-       #timeframe = CurrentWeekTimeframe()
-
-       #gc = GerritCollector(alias="roo",
-       #                     url="ssh://review.openstack.org:29418",
-       #                     login="saschpe")
-
-       #bnc = BugzillaCollector(alias="bnc",
-       #                        url="https://bugzilla.novell.com",
-       #                        login="saschpe",
-       #                        password="wioX9qud")
-
-       #collectors = []
-       #print gc.collect(timeframe)
-       #for plugin in plugins:
-       #    result_json = plugin.collect()
-       #    print result_json
+    def generate(self):
         pass
 
-    def _generate(self):
+    def edit(self):
         pass
 
-    def _edit(self):
-        pass
-
-    def _send(self):
+    def send(self):
         pass
 
     def main(self, argv):
         parser = argparse.ArgumentParser()
         parser.add_argument("--version", action="version",
                             version="rapport {0}".format(rapport.__version__))
+        #parser.add_argument('--verbose', '-v', action='count')
+
         subparsers = parser.add_subparsers(title="commands")
 
         parser_collect = subparsers.add_parser("collect", help="collect activities from various sources")
         parser_collect.add_argument("source", nargs="?", help="specific source")
-        parser_collect.set_defaults(func=self._collect)
+        parser_collect.set_defaults(func=CLI.collect(self))
 
        #parser_generate = subparsers.add_parser("generate", help="generate template from collected data")
        ##parser_generate.add_argument("source", nargs="?", help="specific source")
        ##parser_create.add_argument("-t", "--type", default="text", nargs="?", help="work report type (text, html)")
-       #parser_generate.set_defaults(func=self._generate)
+       #parser_generate.set_defaults(func=CLI.generate(self))
 
        #parser_edit = subparsers.add_parser("edit", help="edit report prior to sending")
-       #parser_edit.set_defaults(func=self._edit)
+       #parser_edit.set_defaults(func=CLI.edit(self))
 
        #parser_send = subparsers.add_parser("send", help="send work report")
        ##parser_generate.add_argument("destination", nargs="+", help="")
-       #parser_send.set_defaults(func=self._send)
+       #parser_send.set_defaults(func=CLI.send(self))
 
-       #parser_help = subparsers.add_parser("help", help="show this help")
-       #parser_help.set_defaults(func=lambda args: parser.print_help())
+        parser_help = subparsers.add_parser("help", help="show this help")
+        parser_help.set_defaults(func=lambda args: parser.print_help())
 
         args = parser.parse_args()
-        args.func(args)
 
 
 def main():
    #try:
-        RapportCLI().main(sys.argv[1:])
+        CLI().main(sys.argv[1:])
    #except Exception as e:
    #    print >> sys.stderr, e
    #    sys.exit(1)
