e009ec6b105c66b1b8cee7bb52e03927e507f7d1
Sascha Peilicke <saschpe@suse.de>
2013-04-15 15:16:15 -0700

    Add "show", "list" and "email xdg" CLI commands
    The former two are for listing and displaying reports. The latter allows
    to compose via xdg-email (and thus open up your preferred mail in
    KDE/GNOME/XFCE/etc.).

--- rapport/cli.py
+++ rapport/cli.py
@@ -27,13 +27,16 @@ if sys.version_info > (3, 3):
 else:
     import futures
 
-# Custom hack for running rapport/cli inside the development tree:
+# Custom hack for being able to "import rapport" (for __version__) inside the
+# development tree:
 if __name__ == "__main__":
     sys.path.append(os.getcwd())
 
 import rapport
 import rapport.config
+import rapport.email
 import rapport.plugin
+import rapport.report
 import rapport.template
 import rapport.timeframe
 
@@ -63,40 +66,56 @@ class CLI(object):
                 except Exception as e:
                     print >>sys.stderr, "Failed plugin {0}:{1}: {2}!".format(plugin, plugin.alias, e)
 
-        # TODO: Generate mail template
-        # TODO: refactor this into appropriate modules:
-
+        # Create new report:
         render_date = self.timeframe.end
-        report_path = os.path.expanduser(os.path.join("~", ".rapport", "reports", render_date.isoformat()))
-        if not os.path.exists(report_path):
-            os.makedirs(report_path)
+        report_path = rapport.report.create_report(render_date)
 
+        # Render mail templates:
         template_email_body = rapport.template.get_template("body", type="email")
-        email_body = template_email_body.render({"plugins": self.plugins, "results": results})
+        email_body = template_email_body.render({"plugins": self.plugins,
+                                                 "results": results})
         email_body_file = os.path.join(report_path, "email.body.text")
         with open(email_body_file, "w") as report:
             report.write(email_body)
 
         template_email_subject = rapport.template.get_template("subject", type="email")
-        email_subject = template_email_subject.render({"login": rapport.config.get("user", "login"), "date": render_date.date().isoformat()})
+        email_subject = template_email_subject.render({"login": rapport.config.get("user", "login"),
+                                                       "date": render_date.date().isoformat()})
         email_subject_file = os.path.join(report_path, "email.subject.text")
         with open(email_subject_file, "w") as report:
             report.write(email_subject)
 
+        # That's it:
         print "Your work report ({0}):\n{1}".format(email_body_file, email_body)
 
-       ## Print results sorted by plugin appearance in config file (i.e. init order):
-       #for plugin in self.plugins:
-       #    try:
-       #        print results[plugin]
-       #    except KeyError as e:
-       #        # A missing result for plugins means an exception happened
-       #        # above, which already printed an error message, thus:
-       #        pass
+    def list(self, args):
+        for report in rapport.report.list_reports():
+            print report
+
+    def show(self, args):
+        report = rapport.report.get_report(args.report)
+        if args.raw:
+            print report
+        else:
+            print report["email.body.text"]
 
 
    #def edit(self, args):
    #    pass
+
+   #def email_send(self, args):
+   #    pass
+
+   #def email_compose(self, args):
+   #    pass
+
+    def email_xdg(self, args):
+        report = rapport.report.get_report(args.report)
+        rapport.email.xdg_compose(to=rapport.config.get("email", "to"),
+                                  subject=report["email.subject.text"],
+                                  body=report["email.body.text"],
+                                  cc=rapport.config.get("email", "cc"),
+                                  bcc=rapport.config.get("email", "bcc"))
 
     def main(self):
         parser = argparse.ArgumentParser()
@@ -105,9 +124,24 @@ class CLI(object):
 
         parser_create = subparsers.add_parser("create", help="create work report")
         parser_create.set_defaults(func=self.create)
-
+        parser_list = subparsers.add_parser("list", help="list work reports")
+        parser_list.set_defaults(func=self.list)
+        parser_show = subparsers.add_parser("show", help="display a work report")
+        parser_show.add_argument("-r", "--raw", action="store_true", help="display the raw report data")
+        parser_show.add_argument("report", nargs="?", default=None)
+        parser_show.set_defaults(func=self.show)
        #parser_edit = subparsers.add_parser("edit", help="edit report prior to sending")
        #parser_edit.set_defaults(func=self.edit)
+
+        parser_email = subparsers.add_parser("email", help="e-mail functionality")
+        email_subparsers = parser_email.add_subparsers(title="email commands")
+       #parser_email_send = email_subparsers.add_parser("send")
+       #parser_email_send.set_defaults(func=self.email_send)
+       #parser_email_compose = email_subparsers.add_parser("compose")
+       #parser_email_compose.set_defaults(func=self.email_compose)
+        parser_email_xdg = email_subparsers.add_parser("xdg", help="use xdg-email to compose, i.e. use your preferred mailer under KDE/GNOME/XFCE/etc.")
+        parser_email_xdg.add_argument("report", nargs="?", default=None)
+        parser_email_xdg.set_defaults(func=self.email_xdg)
 
         parser_help = subparsers.add_parser("help", help="show this help")
         parser_help.set_defaults(func=lambda args: parser.print_help())
