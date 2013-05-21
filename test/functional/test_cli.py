f79911895bfbe01ac9fefd189f289bd716871f2a
Sascha Peilicke <saschpe@gmx.de>
2013-05-21 09:55:43 +0200

    Revert "Drop sys.path hack"
    This reverts commit 4b14c04000ed2781be33cf384842735d3981c714.
    
    Unfortunately, I still need it from time to time and haven't yet found a
    better solution.

--- test/functional/test_cli.py
+++ test/functional/test_cli.py
@@ -26,7 +26,7 @@ class CLIFunctionalTestCase(unittest.TestCase):
         self.result = "{0}\n".format(__version__)
 
     def test_call_rapport_cli(self):
-        args = [sys.executable, "-m", "rapport.cli", "--version"]
+        args = [sys.executable, "rapport/cli.py", "--version"]
         self.assertEqual(silent_popen(args), self.result)
 
     def test_call_script_wrapper(self):
